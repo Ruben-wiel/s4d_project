5b9cfe7ec78fdc0ed30960fc22aeaa252f4168cf
Jente <jente.otte@live.nl>
2020-05-12 12:52:57 +0200

    Paths naar elke pagina

--- s4d_webapp/website/urls.py
+++ s4d_webapp/website/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.home, name='web-home'),
     path('about/', views.about, name='web-overons'),
+    path('posts/', views.posts, name='web-posts'),
+    path('account/', views.account, name='web-account'),
 ]
