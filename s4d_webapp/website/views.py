fde8d7c3ff43b4937dd42a09b66df2b94a7198f7
Jente <jente.otte@live.nl>
2020-05-12 10:42:10 +0200

    Routing, pagina's

--- s4d_webapp/website/views.py
+++ s4d_webapp/website/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def home(request):
+    return HttpResponse('<h1>Home Pagina</h1>')
+
+def about(request):
+    return HttpResponse('<h1>Over Ons</h1>')
+
+def posts(request):
+    return HttpResponse('<h1>Advertenties</h1>')
+
+def account(request):
+    return HttpResponse('<h1>User Portal</h1>')
