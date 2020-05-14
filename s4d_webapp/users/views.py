0674846a3d9c644ee12a9cbc97238144c10e67bc
Ramone <ramonvanderstoop@gmail.com>
2020-05-14 12:42:59 +0200

    comments toegevoegd, register improved, base.html aangepast voor message

--- s4d_webapp/users/views.py
+++ s4d_webapp/users/views.py
@@ -1,6 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
 
 def register(request):
-    form =  UserCreationForm()
+    #checkt of de usercreation al is ingevuld, als dat zo is ga je naar de homepage, zo niet returnt pagina terug
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            #f'x' is een Fstring
+            messages.success(request, f'Account created for {username}!')
+            return redirect('web-home')
+    else:
+        form =  UserCreationForm()
     return render(request, 'users/register.html', {'form': form})
