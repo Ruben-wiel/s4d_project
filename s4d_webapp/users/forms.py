984306743fdeff0fae0da17a764b32163dfb2660
Ramone <ramonvanderstoop@gmail.com>
2020-05-18 14:48:50 +0200

    profiel update form

--- s4d_webapp/users/forms.py
+++ s4d_webapp/users/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
-
+from .models import Profile
 
 class UserRegisterForm(UserCreationForm):
     email = forms.EmailField()
@@ -17,3 +17,20 @@ class UserRegisterForm(UserCreationForm):
 
 # These are the fields that are going to be shown on our form.
         fields = ['username', 'email', 'phone', 'password1', 'password2']
+
+#"a model form allows us to create a form that will work with a specific database model
+#in this case we want a form that will update our user model."
+#hiermee kan de profile geupdate worden.
+class UserUpdateForm(forms.ModelForm):
+    email = forms.EmailField()
+
+    class Meta:
+        model = User
+        fields = ['username', 'email']
+
+
+#hiermee kan de profile-image geupdate worden.
+class ProfileUpdateForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ['image']
--- s4d_webapp/users/views.py
+++ s4d_webapp/users/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
-from .forms import UserRegisterForm
+from .forms import UserRegisterForm, UserUpdateForm, ProfileUpdateForm
+
 
 def register(request):
     #checkt of de usercreation al is ingevuld, als dat zo is ga je naar de homepage, zo niet returnt pagina terug
@@ -17,6 +18,27 @@ def register(request):
         form =  UserRegisterForm()
     return render(request, 'users/register.html', {'form': form})
 
+#forms u_form en p_form zijn toegevoegd aan de profile view
 @login_required
 def profile(request):
-    return render(request, 'users/profile.html')
+    #post request met de data van de form, door de instance=request.user, zorgt er voor
+    #dat er daadwerkelijk ook iets wordt aangepast in de database
+    if request.method == 'POST':
+        u_form  = UserUpdateForm(request.POST, instance=request.user)
+        p_form = ProfileUpdateForm(request.POST,
+                                   request.FILES, #is voor de image data
+                                   instance=request.user.profile)
+        if u_form.is_valid() and u_form.is_valid(): # als beide forms valid zijn wordt de database pas opgeslagen.
+            u_form.save()
+            p_form.save()
+            messages.success(request, f'Je profiel is bijgewerkt!')
+            return redirect('profile')
+    else:
+        u_form = UserUpdateForm(instance=request.user)
+        p_form = ProfileUpdateForm(instance=request.user.profile)
+
+    context = {
+        'u_form': u_form,
+        'p_form': p_form
+    }
+    return render(request, 'users/profile.html', context)
