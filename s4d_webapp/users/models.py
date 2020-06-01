c6a2c35e869338d06f8ce861c8791c8c17da93fe
Ruben van de Wiel <ruben.wiel@hotmail.com>
2020-06-01 13:12:57 +0200

    about_us_page

--- s4d_webapp/users/models.py
+++ s4d_webapp/users/models.py
@@ -1,10 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
 from PIL import Image
-<<<<<<< Updated upstream
-
-=======
->>>>>>> Stashed changes
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
